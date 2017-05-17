289490af794d569791da6a0e9376c4237becd37f
Pauline Dessingue <pdessingue@norsys.fr>
2017-05-17 10:11:29 +0200

    refactor(Services): suppression des classes inutiles pour la pratique

--- src/main/java/com/norsys/service/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.norsys.service.exception;
-
-
-public class AuthenticationException extends Exception {
-
-    public AuthenticationException() {
-        super("AuthenticationException : nombre maximal de tentatives atteint.");
-    }
-}
--- src/test/java/com/norsys/service/ConsommationServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.norsys.service;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-
-public class ConsommationServiceTest {
-
-}
